ebbb3c5eed509c98d1f7d55062330764f7879aef
lauralop11 <lauralop_11@hotmail.com>
2025-02-14 10:24:04 -0500

    end styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,7 @@
 export const Card = ({ title, content, list }) => {
   return(
-    <div className="m-5  shadow-lg list-decimal"> 
-      <h3 className=" text-seconde font-xl font-bold p-4 font-stretch-expanded">{title}</h3>
+    <div className="m-5  shadow-lg list-decimal md:grid md:grid-rows-[104px_auto_170px] md:min-h-[524px]"> 
+      <h3 className=" text-seconde font-xl font-bold p-4 font-stretch-expanded text-center">{title}</h3>
       <p className="font-[verdana] p-4 text-gray-700 ">{content}</p>   
         <ul className="list-disc border-2 p-6 border-seconde bg-seconde rounded-b-md font-[verdana] text-white">
         {
--- src/components/Navbar/Navegation.jsx
+++ src/components/Navbar/Navegation.jsx
@@ -12,18 +12,18 @@ const Navbar = () => {
   const textDecoration = 'underline underline-offset-4'
 return(
   
-  <nav className="navbar flex font-playfair-display sticky top-0 z-20 ">
-    <div className="navbar-logo flex w-full p-4 ">
+  <nav className="navbar flex font-playfair-display sticky top-0 z-20 shadow">
+    <div className="navbar-logo flex w-full md:w-1/2 p-4 ">
       <img src={logo} alt="Cosmet+IQ logo" className="w-10 h-10 self-center"/>
       <div className="text-white flex flex-col justify-center">
         <p className="text-2xl font-light ">COSMET+IQ</p>
         <p className='-mt-1 font-stretch-expanded italic'>Donde la ciencia encuentra la belleza</p>
       </div>
     </div>
-    <ul className="navbar-list md:flex hidden justify-rigth bg-white h-full">
+    <ul className="navbar-list md:flex hidden justify-rigth bg-white h-full md:w-1/2 mx-auto justify-around ">
       {
         menuRoutes.map((item, index) => 
-          <li key={index} className='self-center' >
+          <li key={index} className='self-center cursor-pointer md:hover:underline' >
             <Link to={item.to} smooth={true} duration={500} >{item.text} </Link>
           </li>
           )
